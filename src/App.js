637f062ec3dc58e30c244e054afaf5a11bff64d0
Marbowle <mahomegol@gmail.com>
2022-11-03 13:18:30 +0100

    Delete unnecessery files and add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+import React from "react";
 import "./App.css";
 import Weather from "./Weather";
 import Forecast from "./Forecast";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Weather />
-      </header>
-      <footer className="App-forecast">
-        <Forecast />
-      </footer>
-      <p><a href="https://github.com/Marbowle/weather-app-react">Open-source code</a> by Marta Domagała</p>
-      
+      <div className="container">
+        <header className="App-header">
+          <Weather />
+        </header>
+        <footer className="App-forecast">
+          <Forecast />
+        </footer>
+        <p>
+          <a
+            href="https://github.com/Marbowle/weather-app-react"
+            target="_blank"
+          >
+            Open-source code
+          </a>{" "}
+          by Marta Domagała
+        </p>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -2,36 +2,42 @@ import React from "react";
 import "./Weather.css";
 export default function Weather() {
   return (
-    <div className="conatainer m-3">
-      <div className="row">
-        <div className="col-6"></div>
+    <div className="conatainer">
+      <div class="row">
         <form>
-          <input type="search" placeholder="Type a city" />
-          <input type="submit" value="Search" />
-          <input type="submit" value="Current" />
+          <div className="col-6">
+            <input type="search" placeholder="Type a city" />
+          </div>
+          <div className="col-4">
+            <input type="submit" value="Search" />
+          </div>
+          <div className="col-2">
+            <input type="submit" value="Current" />
+          </div>
         </form>
-        <h1 className="m-4">Stockholm</h1>
-        <div className="col-6 description">
-          <ul>
-            <li>
-              Humidity : {""}
-              <span>20</span>
-            </li>
-            <li>
-              Air Pressure : {""}
-              <span>1013 hPa</span>
-            </li>
-            <li>
-              Chance of Rain : {""}
-              <span>0 %</span>
-            </li>
-            <li>
-              Wind Speed : {""}
-              <span>30 km/h</span>
-            </li>
-          </ul>
-        </div>
       </div>
+      <h1>Stockholm</h1>
+      <div className="col-6 description">
+        <ul>
+          <li>
+            Humidity : {""}
+            <span>20</span>
+          </li>
+          <li>
+            Air Pressure : {""}
+            <span>1013 hPa</span>
+          </li>
+          <li>
+            Chance of Rain : {""}
+            <span>0 %</span>
+          </li>
+          <li>
+            Wind Speed : {""}
+            <span>30 km/h</span>
+          </li>
+        </ul>
+      </div>
+
       <div>
         <img
           src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
